16fbb0e7ceb27447f06d41aa7deb8395bea167da
aldacelio <aldacelio@alu.ufc.br>
2025-01-18 06:47:40 -0300

    fix: correção do cors

--- src/main/java/com/landingpage/backend/controller/DadosController.java
+++ src/main/java/com/landingpage/backend/controller/DadosController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://landing-page-contrata-frontend-i6frcp5lg-aldacelios-projects.vercel.app")
 public class DadosController {
 
     @Autowired
